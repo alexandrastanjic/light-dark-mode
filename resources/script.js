79c45c682d84763ff6db55b026dd6b7c7d21172d
Alexandra S <alexandrastanjic@gmail.com>
2021-01-21 12:53:44 +0200

    Finish UI and start with logic

---
+++ resources/script.js
@@ -0,0 +1,19 @@
+
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+
+function switchTheme(event) {
+  if (event.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    darkMode();
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    lightMode();
+ }
+}
+toggleSwitch.addEventListener('change', switchTheme);
